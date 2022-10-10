86ef37d91acc764b17d5bca94983545bda87030e
fabioflores <fabioflores021@gmail.com>
2022-10-10 13:45:58 -0600

    Modificando componente navbar

---
+++ fronted/src/components/Navbar/Drawer.jsx
@@ -0,0 +1,43 @@
+import { React, useState } from 'react'
+import {
+  Drawer,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material'
+import { Menu } from '@mui/icons-material'
+import { navItems } from './Navbar.jsx'
+
+const DrawerComp = () => {
+  const [openDrawer, setOpenDrawer] = useState(false)
+  const newNavItems = navItems.concat(['Login', 'Registrarse'])
+  return (
+    <>
+      <Drawer
+        sx={{ background: '#fff5ed' }}
+        open={openDrawer}
+        onClose={() => setOpenDrawer(false)}
+      >
+        <List sx={{ background: '#fff5ed' }}>
+          {newNavItems.map((item, index) => (
+            <ListItemButton onClick={() => setOpenDrawer(false)} key={index}>
+              <ListItemIcon>
+                <ListItemText>{item}</ListItemText>
+              </ListItemIcon>
+            </ListItemButton>
+          ))}
+        </List>
+      </Drawer>
+      <IconButton
+        sx={{ color: '#16382c', marginTop: '40px', marginLeft: 'auto' }}
+        onClick={() => setOpenDrawer(!openDrawer)}
+      >
+        <Menu />
+      </IconButton>
+    </>
+  )
+}
+
+export default DrawerComp
